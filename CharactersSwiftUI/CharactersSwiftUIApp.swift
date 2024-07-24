2dca84f2459a06905f72acefa200078ae1dd53f7
Meltem <meltemak8@gmail.com>
2024-07-24 13:32:04 +0200

    Initial Commit

---
+++ CharactersSwiftUI/CharactersSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CharactersSwiftUIApp.swift
+//  CharactersSwiftUI
+//
+//  Created by Meltem Uğurlu on 24.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CharactersSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
